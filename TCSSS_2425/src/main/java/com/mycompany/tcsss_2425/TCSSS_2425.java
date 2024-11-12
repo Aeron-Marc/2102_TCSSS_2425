e46f02dd41008e883d5162c6bbdaacc7bd40574e
Aeron Marc <saeronmarc@gmail.com>
2024-11-12 21:22:10 +0800

    Test

---
+++ TCSSS_2425/src/main/java/com/mycompany/tcsss_2425/TCSSS_2425.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tcsss_2425;
+
+/**
+ *
+ * @author leand
+ */
+public class TCSSS_2425 {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
